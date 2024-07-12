0a0587656c085bfdafb392ce2ac45b634fb12d76
fzhnf <faizahnaf52@gmail.com>
2024-07-12 18:08:49 +0800

    refactor neogen

--- lua/custom/plugins/neogen.lua
+++ lua/custom/plugins/neogen.lua
@@ -1,17 +1,31 @@
 return {
   {
     'danymat/neogen',
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter', lazy = true },
+
+      { 'L3MON4D3/LuaSnip', lazy = true },
+    },
     cmd = 'Neogen',
     opts = {
       snippet_engine = 'luasnip',
     },
     keys = {
       {
-        '<leader>cn',
+        '<leader>tg',
         function()
-          require('neogen').generate()
+          vim.ui.select({ 'any', 'func', 'class', 'type', 'file' }, {
+            prompt = 'select annotation type:',
+            format_item = function(item)
+              return 'generate ' .. item
+            end,
+          }, function(input)
+            require('neogen').generate { type = input }
+          end)
         end,
-        desc = 'Generate Annotations (Neogen)',
+        mode = {'n','v'},
+        desc = '[G]enerate annotation',
+        silent = true,
       },
     },
   },
