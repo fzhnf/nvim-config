44b6c2fbb8a4759acb976130ea2cd4fe08de7f6b
fzhnf <faizahnaf52@gmail.com>
2024-07-12 18:07:24 +0800

    refactor toggleterm

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -3,9 +3,23 @@ return {
     'akinsho/toggleterm.nvim',
     opts = {},
     keys = {
-      { '<M-[>', '<cmd>ToggleTerm direction=horizontal<cr>', mode = { 't', 'n' }, desc = 'Open terminal below' },
-      { '<M-]>', '<cmd>ToggleTerm direction=vertical size=45<cr>', mode = { 't', 'n' }, desc = 'Open terminal right' },
-      { '<M-\\>', '<cmd>ToggleTerm direction=float<cr>', mode = { 't', 'n' }, desc = 'Open floating terminal' },
+      { '<A-[>', '<cmd>ToggleTerm direction=horizontal<cr>', mode = { 't', 'n' }, desc = 'Open terminal below' },
+      { '<A-]>', '<cmd>ToggleTerm direction=vertical size=60<cr>', mode = { 't', 'n' }, desc = 'Open terminal right' },
+      { '<A-\\>', '<cmd>ToggleTerm direction=tab<cr>', mode = { 't', 'n' }, desc = 'Open  terminal in tab mode' },
+      { '<A-CR>', '<cmd>ToggleTerm direction=float<cr>', mode = { 't', 'n' }, desc = 'Open floating terminal ' },
+      -- { '<leader>gl', desc = 'Open lazygit' },
+      {
+        '<leader>gl',
+        function()
+          require('toggleterm').setup()
+          local Terminal = require('toggleterm.terminal').Terminal
+          local lazygit = Terminal:new { cmd = 'lazygit', hidden = true, direction = 'float', close_on_exit = true }
+
+          lazygit.dir = vim.fn.expand '%:p:h' -- current working directory
+          lazygit:toggle()
+        end,
+        desc = 'Open lazygit',
+      },
     },
   },
 }
