f9954935daa00bed64d89523bb662efb258f02e4
fzhnf <faizahnaf52@gmail.com>
2024-08-10 23:21:18 +0800

    feature to use plugin's format feature on save

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -1,6 +1,21 @@
 return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'neovim/nvim-lspconfig', lazy = true },
-  ft = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact', 'vue' },
-  opts = {},
+  ft = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
+  opts = {
+    settings = {
+      separate_diagnostic_server = false,
+    },
+  },
+  config = function(_, opts)
+    require('typescript-tools').setup(opts)
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      group = vim.api.nvim_create_augroup('TSToolsFormat', {}),
+      pattern = '*.ts,*.tsx,*.jsx,*.js',
+      callback = function()
+        vim.cmd 'TSToolsAddMissingImports sync'
+        vim.cmd 'TSToolsOrganizeImports sync'
+      end,
+    })
+  end,
 }
